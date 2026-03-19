089fdf349ee009ecd2f856449bd80bc7f447c88d
CmuriloDev <murilo.portela.dev@gmail.com>
2026-03-19 18:59:48 -0300

    feat: load dataset subset from huggingface

---
+++ dataset.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+
+
+def load_data():
+    dataset = load_dataset("bentrevett/multi30k", split="train[:1000]")
+
+    pairs = []
+
+    for item in dataset:
+        src = item["en"]
+        tgt = item["de"]
+        pairs.append((src, tgt))
+
+    return pairs
